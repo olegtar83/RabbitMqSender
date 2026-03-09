0a309ec59d943f03196bc55c0b87c079c9ee90bf
Oleg Tarnarutsky <>
2026-03-09 14:01:08 +0300

    optimizations

--- RabbitMqSender/Consumers/PaymentConsumer.cs
+++ RabbitMqSender/Consumers/PaymentConsumer.cs
@@ -6,6 +6,7 @@ using RabbitMqSender.DataClasses;
 using RabbitMqSender.DataClasses.Entities;
 using RabbitMqSender.DataClasses.Enums;
 using RabbitMqSender.Extensions;
+using System.Buffers;
 using System.Text;
 using System.Text.Json;
 
@@ -21,22 +22,37 @@ namespace RabbitMqSender.Consumers
         private readonly HttpClient _client = httpClientFactory.CreateClient("InvoiceClient");
         private readonly ObjectPool<StringBuilder> _pool = new DefaultObjectPool<StringBuilder>(
                 new StringBuilderPooledObjectPolicy(),
-                20);
+                Environment.ProcessorCount * 2);
+        private static readonly JsonSerializerOptions _jsonOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        private readonly Dictionary<string, PaymentStatus> _statuses = dbContext.
+                                PaymentStatus.AsNoTracking().ToDictionary(x => x.Status, x => x);
 
         public async Task Consume(ConsumeContext<PaymentRequest> context)
         {
-            var statuses = await _dbContext.PaymentStatus.ToListAsync();
-            var statusDictionary = statuses.ToDictionary(x => x.Status, x => x);
+            byte[]? jsonBytes = null;
+            byte[]? rentedBuffer = null;
+            try
+            {
+                rentedBuffer = ArrayPool<byte>.Shared.Rent(1024);
+                using var utf8JsonWriter = new Utf8JsonWriter(new MemoryStream(rentedBuffer));
+                JsonSerializer.Serialize(utf8JsonWriter, context.Message, _jsonOptions);
+                jsonBytes = rentedBuffer.AsSpan(0, (int)utf8JsonWriter.BytesCommitted).ToArray();
+            }
+            finally
+            {
+                if (rentedBuffer != null)
+                    ArrayPool<byte>.Shared.Return(rentedBuffer);
+            }
 
             var payment = new Payment()
             {
-                JsonMessage = JsonSerializer.Serialize(context.Message),
+                JsonMessage = Encoding.UTF8.GetString(jsonBytes),
                 ReceivedAt = DateTime.UtcNow,
-                PaymentStatus = statusDictionary![Status.Received.GetDescription()]!
+                PaymentStatus = _statuses![Status.Received.GetDescription()]!
             };
 
-            await _dbContext.Payments.AddAsync(payment);
-            await _dbContext.SaveChangesAsync();
+            await _dbContext.Payments.AddAsync(payment, context.CancellationToken);
+            await _dbContext.SaveChangesAsync(context.CancellationToken);
 
             var xmlString = BuildXml(context.Message);
 
@@ -46,16 +62,16 @@ namespace RabbitMqSender.Consumers
                 var response = await _client.PostAsync("", httpContent);
 
                 payment.PaymentStatus = response.IsSuccessStatusCode
-                    ? statusDictionary![Status.Sent.GetDescription()]!
-                    : statusDictionary![Status.Error.GetDescription()]!;
+                    ? _statuses![Status.Sent.GetDescription()]!
+                    : _statuses![Status.Error.GetDescription()]!;
             }
             catch (HttpRequestException httpEx)
             {
-                payment.PaymentStatus = statusDictionary![Status.Error.GetDescription()]!;
+                payment.PaymentStatus = _statuses![Status.Error.GetDescription()]!;
                 _logger.LogError(httpEx, "Failed to send payment");
             }
 
-            await _dbContext.SaveChangesAsync();
+            await _dbContext.SaveChangesAsync(context.CancellationToken);
         }
 
         private string BuildXml(PaymentRequest paymentRequest)
@@ -67,10 +83,10 @@ namespace RabbitMqSender.Consumers
 
                 sb.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
                 sb.Append("<InvoicePayment>");
-                sb.Append("<Id>").Append(paymentRequest.Request.Id).Append("</Id>");
+                AppendFormattedValue(sb, paymentRequest.Request.Id, "Id");
                 sb.Append("<Debit>").Append(paymentRequest.DebitPart.AccountNumber).Append("</Debit>");
                 sb.Append("<Credit>").Append(paymentRequest.CreditPart.AccountNumber).Append("</Credit>");
-                sb.Append("<Amount>").Append(paymentRequest.DebitPart.Amount).Append("</Amount>");
+                AppendFormattedValue(sb, paymentRequest.DebitPart.Amount, "Amount");
                 sb.Append("<Currency>").Append(paymentRequest.DebitPart.Currency).Append("</Currency>");
                 sb.Append("<Details>").Append(paymentRequest.Details).Append("</Details>");
 
@@ -85,6 +101,24 @@ namespace RabbitMqSender.Consumers
             {
                 _pool.Return(sb);
             }
+        }
+
+    private static void AppendFormattedValue<T>(StringBuilder sb, T value, string elementName)
+        where T : ISpanFormattable
+        {
+            sb.Append('<').Append(elementName).Append('>');
+
+            Span<char> buffer = stackalloc char[64];
+            if (value.TryFormat(buffer, out int charsWritten, default, null))
+            {
+                sb.Append(buffer[..charsWritten]);
+            }
+            else
+            {
+                sb.Append(value.ToString());
+            }
+
+            sb.Append('<').Append('/').Append(elementName).Append('>');
         }
     }
 
