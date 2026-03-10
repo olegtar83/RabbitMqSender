5d00d9c852aa5eb93fc014fbf2c0af005747776d
Oleg Tarnarutsky <>
2026-03-10 08:32:15 +0300

    optimizations

--- RabbitMqSender/Extensions/DictionaryExtensions.cs
+++ RabbitMqSender/Extensions/DictionaryExtensions.cs
@@ -2,7 +2,7 @@
 {
     public static class DictionaryExtensions
     {
-        public static TValue? GetValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key)
+        public static TValue? GetValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key) where TKey: notnull
         {
             return dictionary.TryGetValue(key, out var value) ? value : default;
         }
